fd091a336f92700a595df42b46672f92a6b0d263
Justin Palmer <justin@labratrevenge.com>
2012-01-07 13:29:02 -0800

    initial bar tip

--- src/d3.tip.js
+++ src/d3.tip.js
@@ -1,24 +1,29 @@
 d3.svg.tip = function() {
   var orient = 'top',
       id = 'd3-tip',
-      className = 'd3-tip',
-      pad = 5,
-      stemSize = 60;
+      className = d3_svg_className,
+      padding = d3_svg_padding,
+      stemSize = 60,
+      offset = d3_svg_offset,
+      text = d3_svg_text;
 
   function tip(d, i) {
-    var el = d3.select(this),
+    var pad = padding.apply(this, arguments),
+        oset = offset.apply(this, arguments),
+        cnt = text.apply(this, arguments),
+        klass = className.apply(this, arguments),
+        el = d3.select(this),
         doc = d3.select(this.ownerSVGElement),
         group = doc.select('#' + id).empty() ? doc.append('g').attr('id', id) : doc.select('#' + id),
         bounds = doc.node().getBoundingClientRect(),
         symbol = d3.svg.symbol().type('triangle-down').size(stemSize);
     
-    //el.on('mouseout', function() { group.remove() })
+    el.on('mouseout', function() { group.remove() })
 
-    group.classed(className, true).text(' ');
+    group.classed(klass, true).text(' ');
 
     var rect = group.append('rect').attr('transform', 'translate(0,0)').attr('rx', 3).attr('ry', 3),
         stem = group.append('path').attr('d', symbol),
-        cnt = content(d, i, group),
         ebbox = el.node().getBBox(),
         stemBounds = stem.node().getBBox();
 
@@ -27,14 +32,15 @@ d3.svg.tip = function() {
           .text(cnt)
           .attr('text-anchor', 'middle')
           .attr('alignment-baseline', 'middle'),
-          sbbox = str.node().getBBox();
+          sbbox = str.node().getBBox(),
+          rectw = sbbox.width + padding(d, i) * 2,
+          recth = sbbox.height + padding(d, i) * 2;
 
-      rect.attr('width', sbbox.width + pad * 2)
-        .attr('height', sbbox.height + pad * 2)
+      rect.attr('width', rectw).attr('height', recth)
 
       var rbbox = rect.node().getBBox(),
-          x = (ebbox.x - sbbox.width / 2),
-          y = (ebbox.y - rbbox.height) - stemBounds.height;
+          x = (ebbox.x - sbbox.width / 2) + oset[0],
+          y = (ebbox.y - rbbox.height) - stemBounds.height + oset[1];
       
       if(x <= 0) { x = 0 }
       if(x + rbbox.width > bounds.width) { x = bounds.width - rbbox.width }
@@ -43,9 +49,25 @@ d3.svg.tip = function() {
       str.attr('dx', rbbox.width / 2).attr('dy', rbbox.height / 2)
       group.attr('transform', "translate(" + x + "," + y + ")")
       stem.attr('transform','translate(' + rbbox.width / 2 + ',' + (rbbox.height + stemBounds.height / 2) + ')')
-    }    
+    }
+        
   }
 
+  function d3_svg_padding() {
+    return 5;
+  }
+
+  function d3_svg_offset() {
+    return [0, 0];
+  }
+
+  function d3_svg_text() {
+    return ' ';
+  }
+
+  function d3_svg_className() {
+    return 'd3-tip';
+  }
   
   tip.orient = function(v) {
     if (!arguments.length) return orient;
@@ -65,9 +87,21 @@ d3.svg.tip = function() {
     return tip;
   };
 
-  tip.content = function(v) {
-    if (!arguments.length) return content;
-    content = v == null ? v: d3.functor(v);
+  tip.padding = function(v) {
+    if (!arguments.length) return padding;
+    padding = v == null ? v: d3.functor(v);
+    return tip;
+  };  
+
+  tip.offset = function(v) {
+    if (!arguments.length) return offset;
+    offset = v == null ? v: d3.functor(v);
+    return tip;
+  };  
+
+  tip.text = function(v) {
+    if (!arguments.length) return text;
+    text = v == null ? v: d3.functor(v);
 
     return tip;
   };
