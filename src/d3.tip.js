ca18d18cd211d585ca267c665d15f336a59873f5
Justin Palmer <justin@labratrevenge.com>
2012-01-09 07:42:25 -0800

    formatting fixes

--- src/d3.tip.js
+++ src/d3.tip.js
@@ -2,7 +2,7 @@ d3.svg.tip = function() {
   var orient = 'top',
       padding = 5,
       cornerRadius = 2,
-      stemSize = 60,
+      stemSize = 90,
       offset = d3_svg_offset,
       text = d3_svg_text,
       node = make('g');
@@ -13,7 +13,7 @@ d3.svg.tip = function() {
     var tipOffset = offset.apply(this, arguments),
         tipText   = text.apply(this, arguments),
         container = d3.select(node),
-        x, y, stem, backingRect, containerRect;
+        x, y, stem, backingRect, containerRect, stemRect;
     
     // Elements and Bounds
     var doc        = d3.select(this.ownerSVGElement),
@@ -28,83 +28,44 @@ d3.svg.tip = function() {
     container.text(' ').node().appendChild(backing.node())
 
     // The value to show in the tooltip
-    var val     = container.append('text').text(tipText).attr('text-anchor', 'middle').attr('alignment-baseline', 'middle'),
+    var val = container.append('text').text(tipText).attr('text-anchor', 'middle').attr('alignment-baseline', 'middle'),
         valRect = val.node().getBBox();
     
     valRect.width = valRect.width + (padding * 2)
     valRect.height = valRect.height + (padding * 2)
 
-    backing.attr('width', valRect.width).attr('height', valRect.height).attr('rx', cornerRadius).attr('ry', cornerRadius)
+    backing.attr('width', valRect.width)
+      .attr('height', valRect.height)
+      .attr('rx', cornerRadius)
+      .attr('ry', cornerRadius)
+      
     val.attr('dx', valRect.width / 2).attr('dy', valRect.height / 2)
 
-    backingRect = backing.node().getBBox();
+    backingRect = backing.node().getBBox()
+
+    // TODO: stem seems to report the wrong height, so it's never completely flush
+    // against the backing rect.
+    stem = container.append('path').attr('d', d3_svg_stem())
+    stemRect = stem.node().getBBox()
 
     switch(orient) {
       case 'top':
-        stem = container.append('path')
-          .attr('d', d3_svg_stem())
-          .attr('transform', 'translate(' + (backingRect.width / 2) + ',' + backingRect.height + ')');
-                  
-        containerRect = container.node().getBBox()
+        stem.attr('transform', 'translate(' + (backingRect.width / 2) + ',' + backingRect.height + ')');
 
+        containerRect = container.node().getBBox()
         x = targetRect.x + (targetRect.width / 2) - (containerRect.width / 2) + tipOffset[0];
         y = targetRect.y - containerRect.height + tipOffset[1];
         break;
       case 'bottom':
-        // stem = d3.svg.symbol().type('triangle-down').size(stemSize);
-        // stem = d3.select(make('path')).attr('d', stem);
-        // container.node().appendChild(stem.node())
-        var containerRect = container.node().getBBox()
-
+        stem.attr('transform', 'translate(' + (backingRect.width / 2) + ',' + -(stemRect.height / 2) + ')');
+        
+        containerRect = container.node().getBBox()
         x = targetRect.x + (targetRect.width / 2) - (containerRect.width / 2) + tipOffset[0];
-        y = targetRect.y + targetRect.height - tipOffset[1];
+        y = targetRect.y + targetRect.height + stemRect.height - tipOffset[1];
         break;
     }
 
     container.attr('transform', 'translate(' + x + ',' + y + ')')
-    //ele.on('mouseout', function() { container.remove() })
-    // var pad     = padding.apply(this, arguments),
-    //     oset    = offset.apply(this, arguments),
-    //     cnt     = text.apply(this, arguments),
-    //     klass   = className.apply(this, arguments),
-    //     el      = d3.select(this),
-    //     doc     = d3.select(this.ownerSVGElement),
-    //     group   = doc.select('#' + id).empty() ? doc.append('g').attr('id', id) : doc.select('#' + id),
-    //     bounds  = doc.node().getBoundingClientRect(),
-    //     symbol  = d3.svg.symbol().type('triangle-down').size(stemSize),
-    //     rect    = group.append('rect').attr('transform', 'translate(0,0)').attr('rx', 3).attr('ry', 3),
-    //     stem    = group.append('path').attr('d', symbol),
-    //     ebbox   = el.node().getBoundingClientRect(),
-    //     stemBounds = stem.node().getBBox();
-    
-    // //var rect = group.append('rect').attr('transform', 'translate(0,0)').attr('rx', 3).attr('ry', 3);
-    // //el.on('mouseout', function() { group.remove() })
-
-    // group.classed(klass, true) //.text(' ');
-
-    // if(typeof cnt === 'string' || typeof cnt === 'number') {
-    //   var str = group.append('text')
-    //       .text(cnt)
-    //       .attr('text-anchor', 'middle')
-    //       .attr('alignment-baseline', 'middle'),
-    //       sbbox = str.node().getBoundingClientRect(),
-    //       rectw = sbbox.width + padding(d, i) * 2,
-    //       recth = sbbox.height + padding(d, i) * 2;
-
-    //   rect.attr('width', rectw).attr('height', recth)
-    //   var rbbox = rect.node().getBBox(),
-    //       x = (ebbox.right - (ebbox.width / 2) - (rbbox.width / 2) - (stemBounds.width / 2)) + oset[0],
-    //       y = (ebbox.top - rbbox.height) - stemBounds.height + oset[1];
-      
-    //   if(x <= 0) { x = 0 }
-    //   if(x + rbbox.width > bounds.width) { x = bounds.width - rbbox.width }
-    //   if(y <= 0) { y = 0 }
-
-    //   str.attr('dx', rbbox.width / 2).attr('dy', rbbox.height / 2)
-    //   group.attr('transform', "translate(" + x + "," + y + ")")
-    //   stem.attr('transform','translate(' + rbbox.width / 2 + ',' + (rbbox.height + stemBounds.height / 2) + ')')
-    // }
-        
   }
 
   function d3_svg_offset() {
