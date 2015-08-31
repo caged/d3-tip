1ddd7147ee712ed6dcb3ad282d7982ac018eff4b
Michael Malecki <mike@crunch.io>
2015-08-31 12:03:54 -0500

    properly require d3 for npm

--- index.js
+++ index.js
@@ -9,10 +9,7 @@
     define(['d3'], factory)
   } else if (typeof module === 'object' && module.exports) {
     // CommonJS
-    module.exports = function(d3) {
-      d3.tip = factory(d3)
-      return d3.tip
-    }
+    module.exports = factory(require('d3'))
   } else {
     // Browser global.
     root.d3.tip = factory(root.d3)
