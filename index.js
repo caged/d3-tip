6b9791406231f50c4ca156316b0b7bb17a867e3b
Tim van der Horst <tim.vanderhorst@sportsmedglobal.com>
2016-08-11 16:55:26 +1000

    Migrate dependencies to d3 micro libraries

--- index.js
+++ index.js
@@ -8,19 +8,24 @@
 ;(function(root, factory) {
   if (typeof define === 'function' && define.amd) {
     // AMD. Register as an anonymous module with d3 as a dependency.
-    define(['d3'], factory)
+    define([
+      'd3-collection',
+      'd3-selection'
+    ], factory)
   } else if (typeof module === 'object' && module.exports) {
     /* eslint-disable global-require */
     // CommonJS
-    var d3 = require('d3')
-    module.exports = factory(d3)
+    var d3Collection = require('d3-collection'),
+        d3Selection = require('d3-selection')
+    module.exports = factory(d3Collection, d3Selection)
     /* eslint-enable global-require */
   } else {
     // Browser global.
+    var d3 = root.d3
     // eslint-disable-next-line no-param-reassign
-    root.d3.tip = factory(root.d3)
+    root.d3.tip = factory(d3, d3)
   }
-}(this, function(d3) {
+}(this, function(d3Collection, d3Selection) {
   // Public - contructs a new tooltip
   //
   // Returns a tip
@@ -93,7 +98,7 @@
       }
 
       var args =  Array.prototype.slice.call(arguments)
-      d3.selection.prototype.attr.apply(getNodeEl(), args)
+      d3Selection.selection.prototype.attr.apply(getNodeEl(), args)
       return tip
     }
 
@@ -111,7 +116,7 @@
       }
 
       var args = Array.prototype.slice.call(arguments)
-      d3.selection.prototype.style.apply(getNodeEl(), args)
+      d3Selection.selection.prototype.style.apply(getNodeEl(), args)
       return tip
     }
 
@@ -167,7 +172,7 @@
     function d3TipOffset() { return [0, 0] }
     function d3TipHTML() { return ' ' }
 
-    var directionCallbacks = d3.map({
+    var directionCallbacks = d3Collection.map({
           n:  directionNorth,
           s:  directionSouth,
           e:  directionEast,
@@ -244,7 +249,7 @@
     }
 
     function initNode() {
-      var div = d3.select(document.createElement('div'))
+      var div = d3Selection.select(document.createElement('div'))
       div
         .style('position', 'absolute')
         .style('top', 0)
@@ -268,7 +273,7 @@
         // re-add node to DOM
         document.body.appendChild(node)
       }
-      return d3.select(node)
+      return d3Selection.select(node)
     }
 
     // Private - gets the screen coordinates of a shape
@@ -285,7 +290,7 @@
     //
     // Returns an Object {n, s, e, w, nw, sw, ne, se}
     function getScreenBBox() {
-      var targetel   = target || d3.event.target
+      var targetel   = target || d3Selection.event.target
 
       while (targetel.getScreenCTM == null && targetel.parentNode == null) {
         targetel = targetel.parentNode
